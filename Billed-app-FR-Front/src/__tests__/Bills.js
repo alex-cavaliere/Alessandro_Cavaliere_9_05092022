5a074291c8981860e39a3d54c5dd2c89446ea629
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-09-22 11:12:16 +0200

    NewBill test suites

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -1,12 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH} from "../constants/routes.js"
+import {localStorageMock} from "../__mocks__/localStorage.js"
 
 import router from "../app/Router.js";
 
@@ -26,7 +25,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(windowIcon.classList.contains('active-icon')).toBe(true)
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,18 +1,43 @@
 /**
- * @jest-environment jsdom
- */
-
-import { screen } from "@testing-library/dom"
+* @jest-environment jsdom
+*/
+import "@testing-library/dom"
+import { fireEvent } from "@testing-library/dom"
+import { getByTestId } from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import router from "../app/Router"
+import { ROUTES_PATH} from "../constants/routes.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import userEvent from "@testing-library/user-event"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("then, mail icon in vertical layout should be highlighted", async () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const mailIcon = screen.getByTestId('icon-mail')
+      expect(mailIcon.classList.contains('active-icon')).toBe(true)
+    })
+  })
+  describe("When I click on 'Type de depense' ", () => {
+    test('then, I have to chose an option', () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      const expense = screen.getByTestId('expense-type')
     })
   })
 })
